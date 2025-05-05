c6286ba8efbb6114f0345fc838da09c631d59d00
selcanakturk <selcan.yusuf.akturk@gmail.com>
2025-05-05 13:34:25 +0300

    fixes

--- travelouge/settings.py
+++ travelouge/settings.py
@@ -78,6 +78,7 @@ SIMPLE_JWT = {
     "TOKEN_OBTAIN_SERIALIZER": "users.serializers.MyTokenObtainPairSerializer",}
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -85,7 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+   
 ]
 #MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
 
@@ -96,7 +97,11 @@ MIDDLEWARE = [
 #     "http://your-flutter-app.com"  # Flutter Web için
 # ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://reset-password-ui-91pg.vercel.app",
+]
 
 ROOT_URLCONF = 'travelouge.urls'
 
