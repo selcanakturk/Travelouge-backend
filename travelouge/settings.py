eb359e650e9ef0d63fe7c11d6e58f7724c2c15f3
selcanakturk <selcan.yusuf.akturk@gmail.com>
2025-05-11 16:41:16 +0300

    fix

--- travelouge/settings.py
+++ travelouge/settings.py
@@ -104,6 +104,19 @@ CORS_ALLOWED_ORIGINS = [
     "https://reset-password-ui.vercel.app",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 ROOT_URLCONF = 'travelouge.urls'
 
 TEMPLATES = [
